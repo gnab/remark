44bbea33cbb2695dea69a02e9c269d38502c851a
Ole Petter Bang <olepbang@gmail.com>
2012-08-15 22:15:24 +0200

    Update to version 0.4.0.

---
+++ remark.language.js
@@ -0,0 +1,26 @@
+/*
+Language: remark markdown flavor
+Author: Ole Petter Bang <olepbang@gmail.com>
+*/
+
+hljs.LANGUAGES.remark = function(){
+  return {
+    contains: [
+      {
+        className: 'keyword',
+        begin: '^#+[^\n]+',
+        relevance: 10
+      },
+      {
+        className: 'comment',
+        begin: '^---?'
+      },
+      { className: 'string',
+        begin: '^\\w+:'
+      },
+      { className: 'literal',
+        begin: '\\{\\{', end: '\\}\\}'
+      }
+    ]
+  };
+}();
