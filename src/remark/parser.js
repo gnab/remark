f3cd40f225d1d07c9202dc11aaac2af55621909b
Ole Petter Bang <olepbang@gmail.com>
2013-05-14 23:39:56 +0200

    Begin parsing of content classes.

--- src/remark/parser.js
+++ src/remark/parser.js
@@ -1,8 +1,9 @@
 module.exports = Parser;
 
-var regex = replace(/(?:^|\n)(?:code|fences|separator)/, {
-    code: /( {4}[^\n]+\n*)+/
-  , fences: / *(`{3,}|~{3,}) *(?:\S+)? *\n(?:[\s\S]+?)\s*\2 *(?:\n+|$)/
+var regex = replace(/(?:^|\n)(?:content|code|fences|separator)/, {
+    content: /(?:\\)?((?:\.[a-z_\-][a-z\-_0-9]*)+)\[/
+  , code: /( {4}[^\n]+\n*)+/
+  , fences: / *(`{3,}|~{3,}) *(?:\S+)? *\n(?:[\s\S]+?)\s*\3 *(?:\n+|$)/
   , separator: /(---?)(?:\n|$)/
   });
 
@@ -12,29 +13,39 @@ Parser.prototype.parse = function (src) {
   var slides = []
     , slide = {continued: false, source: ''}
     , cap
+    , text
     ;
 
   while (cap = regex.exec(src)) {
 
-    // Code
+    // Content class
     if (cap[1]) {
-      slide.source += src.substring(0, cap.index + cap[0].length);
-      src = src.substring(cap.index + cap[0].length);
+      text = getSquareBracketedText(src, cap.index + cap[0].length);
+      slide.source += surroundWithTag(text, cap[1].substring(1),
+        text.indexOf('\n') === -1 ? 'span' : 'div');
+      src = src.substring(cap.index + cap[0].length + text.length + 1);
       continue;
     }
 
-    // Fences
+    // Code
     if (cap[2]) {
       slide.source += src.substring(0, cap.index + cap[0].length);
       src = src.substring(cap.index + cap[0].length);
       continue;
     }
 
-    // Separator
+    // Fences
     if (cap[3]) {
+      slide.source += src.substring(0, cap.index + cap[0].length);
+      src = src.substring(cap.index + cap[0].length);
+      continue;
+    }
+
+    // Separator
+    if (cap[4]) {
       slide.source += src.substring(0, cap.index);
       slides.push(slide);
-      slide = {continued: cap[3] === '--', source: ''};
+      slide = {continued: cap[4] === '--', source: ''};
       src = src.substring(cap.index + cap[0].length);
       continue;
     }
@@ -52,4 +63,23 @@ function replace (regex, replacements) {
   return new RegExp(regex.source.replace(/\w{2,}/g, function (key) {
     return replacements[key].source;
   }));
+}
+
+function getSquareBracketedText (text, offset) {
+  var depth = 1
+    , pos = offset
+    , chr
+    ;
+
+  while (depth > 0 && pos < text.length) {
+    chr = text[pos++];
+    depth += (chr === '[' && 1) || (chr === ']' && -1) || 0;
+  }
+
+  return depth === 0 && text.substr(offset, pos - offset - 1) || null;
+};
+
+function surroundWithTag (text, className, tag) {
+  return '<' + tag + (className && ' class="' + className + '"' || '') + '>'
+    + text + '</' + tag + '>';
 }
--- test/remark/parser_test.js
+++ test/remark/parser_test.js
@@ -10,48 +10,77 @@ describe('Parser', function () {
     it('should handle multiple slides', function () {
       parser.parse('1\n---\n2\n---\n3').length.should.equal(3);
     });
-    
+
     it('should treat empty source as single slide', function () {
       parser.parse('').length.should.equal(1);
-    });    
+    });
 
     it('should ignore slide separator inside fences', function () {
       parser.parse('1\n---\n```\n---\n```\n---\n3').length.should.equal(3);
     });
+
+    it('should ignore slide separator inside content class', function () {
+      parser.parse('1\n---\n2\n.class[\n---\n]\n---\n3').length.should.equal(3);
+    });
   });
-  
-  describe('mapping source to corresponding slides', function () {    
+
+  describe('mapping source to corresponding slides', function () {
     it('should handle single slide', function () {
       parser.parse('1')[0].source.should.equal('1');
     });
-    
+
     it('should handle multiple slides', function () {
       var slides = parser.parse('1\n---\n2\n---\n3');
-      
+
       slides[0].source.should.equal('1');
       slides[1].source.should.equal('2');
       slides[2].source.should.equal('3');
     });
-    
+
     it('should handle empty source', function () {
       parser.parse('')[0].source.should.equal('');
     });
+  });
 
-    it('should handle code', function () {
+  describe('parsing code', function () {
+    it('should include code', function () {
       var slides = parser.parse('1\n    code\n2\n---\n3\n    code\n4');
 
       slides[0].source.should.equal('1\n    code\n2');
       slides[1].source.should.equal('3\n    code\n4');
     });
 
-    it('should handle fences', function () {
+    it('should ignore content class inside code', function () {
+      parser.parse('    .class[x]')[0].source.should.equal('    .class[x]');
+    });
+  });
+
+  describe('parsing fences', function () {
+    it('should include fences', function () {
       var slides = parser.parse('1\n```\n\n```\n2\n---\n3\n```\n\n```\n4');
 
       slides[0].source.should.equal('1\n```\n\n```\n2');
       slides[1].source.should.equal('3\n```\n\n```\n4');
     });
+
+    it('should ignore content class inside fences', function () {
+      parser.parse('```\n.class[x]\n```')[0].source
+        .should.equal('```\n.class[x]\n```');
+    });
   });
-  
+
+  describe('parsing content classes', function () {
+    it('should convert block content classes', function () {
+      parser.parse('.class[\nx\n]')[0].source
+        .should.equal('<div class="class">\nx\n</div>');
+    });
+
+    it('should convert inline content classes', function () {
+      parser.parse('.class[x]')[0].source
+        .should.equal('<span class="class">x</span>');
+    });
+  });
+
   describe('identifying continued slides', function () {
     it('should not identify normal, preceding slide as continued', function () {
       parser.parse('1\n--\n2\n---\n3')[0].continued.should.be.false;
