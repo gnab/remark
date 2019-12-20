23658f3dd9eb3e7093179c5d4e24a6c0f9cd5f73
Daan van Berkel <daan.van.berkel@alliander.com>
2019-12-20 20:57:59 +0100

    Timer should is in correct state after receiving events

--- src/remark/components/timer/timer.js
+++ src/remark/components/timer/timer.js
@@ -8,56 +8,32 @@ function TimerViewModel (events, element) {
   self.events = events;
   self.element = element;
 
-  self.startTime = null;
-  self.pauseStart = null;
-  self.pauseLength = 0;
+  self.state = self.INITIAL;
 
   element.innerHTML = '0:00:00';
 
-  setInterval(function() {
-      self.updateTimer();
-    }, 100);
+  events.on('startTimer', function () {
+    self.state = self.RUNNING;
+  });
 
-  events.on('start', function () {
-    // When we do the first slide change, start the clock.
-    self.startTime = new Date();
+  events.on('pauseTimer', function () {
+    self.state = self.PAUSED;
   });
 
   events.on('resetTimer', function () {
-    // If we reset the timer, clear everything.
-    self.startTime = null;
-    self.pauseStart = null;
-    self.pauseLength = 0;
-    self.element.innerHTML = '0:00:00';
+    self.state = self.INITIAL;
   });
 
-  events.on('pause', function () {
-    self.pauseStart = new Date();
-  });
 
-  events.on('resume', function () {
-    self.pauseLength += new Date() - self.pauseStart;
-    self.pauseStart = null;
-  });
+  setInterval(function() {
+      self.tick();
+    }, 100);
+
 }
 
-TimerViewModel.prototype.updateTimer = function () {
-  var self = this;
-
-  if (self.startTime) {
-    var millis;
-    // If we're currently paused, measure elapsed time from the pauseStart.
-    // Otherwise, use "now".
-    if (self.pauseStart) {
-      millis = self.pauseStart - self.startTime - self.pauseLength;
-    } else {
-      millis = new Date() - self.startTime - self.pauseLength;
-    }
-
-    var seconds = Math.floor(millis / 1000) % 60;
-    var minutes = Math.floor(millis / 60000) % 60;
-    var hours = Math.floor(millis / 3600000);
-
-    self.element.innerHTML = hours + (minutes > 9 ? ':' : ':0') + minutes + (seconds > 9 ? ':' : ':0') + seconds;
-  }
+TimerViewModel.prototype.tick = function () {
 };
+
+TimerViewModel.prototype.INITIAL = {};
+TimerViewModel.prototype.RUNNING = {};
+TimerViewModel.prototype.PAUSED  = {};
--- test/remark/components/timer_test.js
+++ test/remark/components/timer_test.js
@@ -13,94 +13,65 @@ describe('Timer', function () {
     element = document.createElement('div');
   });
 
-  describe('timer updates', function () {
-    beforeEach(function () {
-      timer = new Timer(events, element);
-    });
-
-    it('should do nothing if the timer has not started', function () {
-      timer.element.innerHTML.should.equal('0:00:00');
-    });
-
-    it('should show progress time if the slideshow has started', function () {
-      // Force a specific start time and update
-      timer.startTime = new Date() - (2*3600000 + 34 * 60000 + 56 * 1000);
-      timer.updateTimer();
-      // Timer output should match forced time
-      timer.element.innerHTML.should.equal('2:34:56');
-    });
-
-    it('should compensate for a pause in progress', function () {
-      // Force a specific start time and update, including an in-progress pause
-      timer.startTime = new Date() - (2*3600000 + 34 * 60000 + 56 * 1000);
-      timer.pauseStart = new Date() - (1*3600000 + 23 * 60000 + 45 * 1000);
-      timer.updateTimer();
-      // Timer output should match forced time
-      timer.element.innerHTML.should.equal('1:11:11');
-    });
-
-    it('should compensate for paused time', function () {
-      // Force a specific start time and update, including a recorded pause
-      timer.startTime = new Date() - (2*3600000 + 34 * 60000 + 56 * 1000);
-      timer.pauseLength = (5 * 60000 + 6 * 1000);
-      timer.updateTimer();
-      // Timer output should match forced time
-      timer.element.innerHTML.should.equal('2:29:50');
-    });
-
-
-    it('should compensate for a pause in progress in addition to previous pauses', function () {
-      // Force a specific start time and update, including a recorded pause
-      // and an in-progress pause
-      timer.startTime = new Date() - (2*3600000 + 34 * 60000 + 56 * 1000);
-      timer.pauseLength = (5 * 60000 + 6 * 1000);
-      timer.pauseStart = new Date() - (1*3600000 + 23 * 60000 + 45 * 1000);
-      timer.updateTimer();
-      // Timer output should match forced time
-      timer.element.innerHTML.should.equal('1:06:05');
-    });
-
-  });
-
   describe('timer events', function () {
     beforeEach(function () {
       timer = new Timer(events, element);
     });
 
-    it('should respond to a start event', function () {
-      events.emit('start');
-      timer.startTime.should.not.equal(null);
+    it('should be in an initial state', function(){
+      timer.state.should.equal(timer.INITIAL);
     });
 
-    it('should reset on demand', function () {
-      timer.startTime = new Date() - (2*3600000 + 34 * 60000 + 56 * 1000);
+    it('should respond to a startTimer event', function () {
+      events.emit('startTimer');
+
+      timer.state.should.equal(timer.RUNNING);
+    });
+
+    it('should respond to a pauseTimer event', function () {
+      events.emit('pauseTimer');
+
+      timer.state.should.equal(timer.PAUSED);
+    });
+
+    it('should respond to a resetTimer event', function () {
       events.emit('resetTimer');
-      timer.element.innerHTML.should.equal('0:00:00');
-      // BDD seems to make this really easy test impossible...
-      // timer.startTime.should.equal(null);
-      // timer.pauseStart.should.equal(null);
-      timer.pauseLength.should.equal(0);
+
+      timer.state.should.equal(timer.INITIAL);
     });
 
-    it('should track pause start end time', function () {
-      timer.startTime = new Date() - (2*3600000 + 34 * 60000 + 56 * 1000);
+    describe('sequence of events', function(){
+      it('should be in a correct state after startTimer, pauseTimer', function () {
+        ['startTimer', 'pauseTimer'].forEach(function(event){
+          events.emit(event);
+        });
 
-      events.emit('pause');
-      timer.pauseStart.should.not.equal(null);
-      timer.pauseLength.should.equal(0);
-    });
+        timer.state.should.equal(timer.PAUSED);
+      });
 
-    it('should accumulate pause duration at pause end', function () {
-      timer.startTime = new Date() - (2*3600000 + 34 * 60000 + 56 * 1000);
-      timer.pauseStart = new Date() - (12 * 1000);
-      timer.pauseLength = 100000;
+      it('should be in a correct state after startTimer, resetTimer', function () {
+        ['startTimer', 'resetTimer'].forEach(function(event){
+          events.emit(event);
+        });
 
-      events.emit('resume');
-      // BDD seems to make this really easy test impossible...
-      //timer.pauseStart.should.equal(null);
-      // Microsecond accuracy is a possible problem here, so
-      // allow a 5 microsecond window just in case.
-      timer.pauseLength.should.be.approximately(112000, 5);
-    });
-  });
+        timer.state.should.equal(timer.INITIAL);
+      });
+
+      it('should be in a correct state after startTimer, pauseTimer, startTimer', function () {
+        ['startTimer', 'pauseTimer', 'startTimer'].forEach(function(event){
+          events.emit(event);
+        });
+
+        timer.state.should.equal(timer.RUNNING);
+      });
+
+      it('should be in a correct state after startTimer, pauseTimer, resetTimer', function () {
+        ['startTimer', 'pauseTimer', 'resetTimer'].forEach(function(event){
+          events.emit(event);
+        });
+
+        timer.state.should.equal(timer.INITIAL);
+      });
+     });
+   });
 });
