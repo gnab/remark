c0e802c6e3b77b59a3f4b43e01e77f504d31be60
Daan van Berkel <daan.van.berkel@alliander.com>
2019-12-21 12:25:42 +0100

    Introduce a chronos structure
    this allows the states to control how much the elapsed time should progress

--- src/remark/components/timer/timer.js
+++ src/remark/components/timer/timer.js
@@ -2,11 +2,12 @@ var utils = require('../../utils');
 
 module.exports = TimerViewModel;
 
-function TimerViewModel (events, element) {
+function TimerViewModel(events, element) {
   var self = this;
 
   self.events = events;
   self.element = element;
+  self.chronos = initialChronos();
 
   self.state = self.INITIAL;
 
@@ -25,15 +26,41 @@ function TimerViewModel (events, element) {
   });
 
 
-  setInterval(function() {
-      self.tick();
-    }, 100);
+  setInterval(function () {
+    self.tick();
+  }, 100);
 
 }
 
 TimerViewModel.prototype.tick = function () {
+  var self = this;
+
+  var now = new Date().getTime();
+  self.chronos.lastTick = self.chronos.currentTick;
+  self.chronos.currentTick = now;
+  self.state.update(self.chronos);
 };
 
-TimerViewModel.prototype.INITIAL = {};
-TimerViewModel.prototype.RUNNING = {};
-TimerViewModel.prototype.PAUSED  = {};
+TimerViewModel.prototype.INITIAL = new State('INITIAL', function (chronos) { /* do nothing */ });
+TimerViewModel.prototype.RUNNING = new State('RUNNING', function (chronos) { var delta = chronos.currentTick - chronos.lastTick; chronos.elapsedTime += delta; });
+TimerViewModel.prototype.PAUSED = new State('PAUSED', function (chronos) { /* do nothing */ });
+
+function initialChronos() {
+  var now = new Date().getTime();
+  return {
+    currentTick: now,
+    lastTick: now,
+    elapsedTime: 0
+  };
+}
+
+function State(identifier, updater) {
+  var self = this;
+
+  self.identifier = identifier;
+  self.updater = updater;
+}
+
+State.prototype.update = function (chronos) {
+  this.updater(chronos);
+};
--- test/remark/components/timer_test.js
+++ test/remark/components/timer_test.js
@@ -18,7 +18,7 @@ describe('Timer', function () {
       timer = new Timer(events, element);
     });
 
-    it('should be in an initial state', function(){
+    it('should be in an initial state', function () {
       timer.state.should.equal(timer.INITIAL);
     });
 
@@ -40,9 +40,9 @@ describe('Timer', function () {
       timer.state.should.equal(timer.INITIAL);
     });
 
-    describe('sequence of events', function(){
+    describe('sequence of events', function () {
       it('should be in a correct state after startTimer, pauseTimer', function () {
-        ['startTimer', 'pauseTimer'].forEach(function(event){
+        ['startTimer', 'pauseTimer'].forEach(function (event) {
           events.emit(event);
         });
 
@@ -50,7 +50,7 @@ describe('Timer', function () {
       });
 
       it('should be in a correct state after startTimer, resetTimer', function () {
-        ['startTimer', 'resetTimer'].forEach(function(event){
+        ['startTimer', 'resetTimer'].forEach(function (event) {
           events.emit(event);
         });
 
@@ -58,7 +58,7 @@ describe('Timer', function () {
       });
 
       it('should be in a correct state after startTimer, pauseTimer, startTimer', function () {
-        ['startTimer', 'pauseTimer', 'startTimer'].forEach(function(event){
+        ['startTimer', 'pauseTimer', 'startTimer'].forEach(function (event) {
           events.emit(event);
         });
 
@@ -66,12 +66,73 @@ describe('Timer', function () {
       });
 
       it('should be in a correct state after startTimer, pauseTimer, resetTimer', function () {
-        ['startTimer', 'pauseTimer', 'resetTimer'].forEach(function(event){
+        ['startTimer', 'pauseTimer', 'resetTimer'].forEach(function (event) {
           events.emit(event);
         });
 
         timer.state.should.equal(timer.INITIAL);
       });
-     });
-   });
+    });
+  });
+
+  describe('tick', function () {
+    beforeEach(function () {
+      timer = new Timer(events, element);
+    });
+
+    it('timer in RUNNING state progresses the elapsed time', function (done) {
+      events.emit('startTimer');
+
+      setTimeout(function () {
+        timer.tick();
+
+        timer.chronos.elapsedTime.should.be.above(0);
+        done();
+      })
+    })
+  });
+});
+
+describe('Timer States', function () {
+  var events
+    , element
+    , timer
+    ;
+
+  beforeEach(function () {
+    events = new EventEmitter();
+    element = document.createElement('div');
+    timer = new Timer(events, element);
+  });
+
+  describe('INITIAL', function () {
+    it('does not increment the elapsed time', function () {
+      var chronos = { currentTick: 1, lastTick: 0, elapsedTime: 0 };
+
+      timer.INITIAL.update(chronos);
+
+      chronos.elapsedTime.should.equal(0);
+    });
+
+  });
+
+  describe('PAUSED', function () {
+    it('does not increment the elapsed time', function () {
+      var chronos = { currentTick: 1, lastTick: 0, elapsedTime: 0 };
+
+      timer.PAUSED.update(chronos);
+
+      chronos.elapsedTime.should.equal(0);
+    });
+  });
+
+  describe('RUNNING', function () {
+    it('does increment the elapsed time by the difference between currentTick and lastTick', function () {
+      var chronos = { currentTick: 5, lastTick: 3, elapsedTime: 0 };
+
+      timer.RUNNING.update(chronos);
+
+      chronos.elapsedTime.should.equal(2);
+    });
+  });
 });
